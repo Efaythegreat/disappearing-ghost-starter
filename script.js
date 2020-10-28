680df48446fc94981d23e2c610265f945628e694
Glitch (yifei-disappearing-ghost-starter) <none>
2020-10-28 00:51:02 +0000

    🌜🍜 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+$(".hide-button").click(function(){
+  $(".ghost-image").fadeOut();
+});
 
+$(".show-button").click(function(){
+  $(".ghost-image").fadeIn();
+});
+
+$(".update-img-button").click(function(){
+  $(".ghost-image").attr("src","https://media.tenor.com/images/5b61a360d83b8fccc05f5060b048c6e1/tenor.gif");
+  $("h1").text("Get Rick Rolled lol")
+});
+
+$(".message-button").click(function(){
+  let msg = $("input").val();
+  $(".message").append(`<p>${msg}<p>`);
+});
+
+$(".name-button").click(function(){
+  let msg = $("input").val();
+  $("h1").text("My name is "+msg);
+  
+});
